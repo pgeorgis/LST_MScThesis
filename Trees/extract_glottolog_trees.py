37b57e8c69bb7a0d90ff8a57bbac1235944cf24f
pgeorgis <60008814+pgeorgis@users.noreply.github.com>
2021-06-24 20:08:31 +0200

    Cleaned code

--- Trees/extract_glottolog_trees.py
+++ Trees/extract_glottolog_trees.py
@@ -1,29 +1,11 @@
 #NOTE: a clone of the Glottolog GitHub repository must be saved in the same directory
 #Link: https://github.com/glottolog/glottolog
 
-import os, re
-from collections import defaultdict
-from pathlib import Path
-local_dir = Path(str(os.getcwd()))
-parent_dir = local_dir.parent
-grandparent_dir = parent_dir.parent
-
-#%%
+#Load Glottolog data
 from pyglottolog import Glottolog
 glottolog = Glottolog('glottolog/.')
 
-
-#Extract Newick trees for each family
-arabic = Glottolog.newick_tree(glottolog, start='arab1395')
-baltoslavic = Glottolog.newick_tree(glottolog, start='balt1263')
-italic = Glottolog.newick_tree(glottolog, start='lati1263')
-polynesian = Glottolog.newick_tree(glottolog, start='poly1242')
-sinitic = Glottolog.newick_tree(glottolog, start='sini1245')
-turkic = Glottolog.newick_tree(glottolog, start='turk1311')
-uralic = Glottolog.newick_tree(glottolog, start='ural1272')
-
-#%%
-#Load list of families and their roots
+#Load list of families and the Glottocodes of their roots
 family_roots = {}
 with open('family_roots.txt') as f:
     f = f.readlines()
@@ -31,11 +13,11 @@ with open('family_roots.txt') as f:
         line = line.strip().split(":")
         family, root = line
         family_roots[family] = root
-        
-#%%   
-#Write each Newick tree to a .tre file
+          
+#Extract each family's Glottolog tree in Newick format and write to a .tre file
 for family in family_roots:
     root = family_roots[family]
+    print(f'Extracting {family} tree...')
     newick_tree = Glottolog.newick_tree(glottolog, start=root)
     with open(f'Gold/{family}.tre', 'w') as f:
         f.write(newick_tree)
