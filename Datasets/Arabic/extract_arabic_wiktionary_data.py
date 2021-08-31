540f1ad401d10b10efd19bb8b818248f6fec6549
pgeorgis <60008814+pgeorgis@users.noreply.github.com>
2021-08-31 23:28:15 +0300

    Updated Arabic data/cognate classes

--- Datasets/Arabic/extract_arabic_wiktionary_data.py
+++ Datasets/Arabic/extract_arabic_wiktionary_data.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import os, re
 from collections import defaultdict
+from openpyxl import load_workbook
 from wiktionaryparser import WiktionaryParser #for extracting Maltese IPA from Wiktionary
 parser = WiktionaryParser()
 from pathlib import Path
@@ -11,7 +12,6 @@ grandparent_dir = parent_dir.parent
 #Load phonetic distance data and auxiliary functions
 os.chdir(str(grandparent_dir) + '/Code')
 from auxiliary_functions import csv_to_dict, strip_ch
-os.chdir(str(grandparent_dir) + '/Code/Distance_Measures/')
 from phonetic_distance import *
 os.chdir(local_dir)
 
@@ -204,12 +204,19 @@ def transcribe_maltese(word, gloss):
     else:
         to_transcribe.append((word, gloss))
         return None
-    
-    
+
+#Load pre-transcribed Maltese words so that they don't need to be extracted 
+#from scratch each time the script is run
+maltese_transcriptions = pd.read_csv('Source/maltese_transcriptions.csv')
+maltese_trascriptions = {maltese_transcriptions.Orthography.to_list()[i]:maltese_transcriptions.IPA.to_list()[i]
+                         for i in range(len(maltese_transcriptions))}
 
 def fix_tr(word, lang, gloss):
     if lang == 'Maltese':
-        return transcribe_maltese(word, gloss)
+        try:
+            return maltese_transcriptions[word]
+        except KeyError:
+            return transcribe_maltese(word, gloss)
     
     else:
         #Fix words using conversion dict
@@ -245,6 +252,7 @@ dialects = ['Arabic (MSA)',
  'Hassaniya (Mauritanian)',
  'Maltese']
 
+#%%
 processed_arabic_data = defaultdict(lambda:{})
 
 j = 0
@@ -256,11 +264,17 @@ for i in arabic_data:
             dialect_entry = entry[dialect]
             
             #If there are masculine and feminine forms for a given word entry, take only the masculine as default
-            dialect_entry = dialect_entry.split(' (m),')[0]
+            #dialect_entry = re.split(' \(m(/f)*\)', dialect_entry)[0]
+            #On revision, don't do this, because some languages take their only
+            #form from the feminine form and it would appear non-cognate if only the
+            #masculine form is listed for other languages, although both exist
             
             #Strip parenthetical annotations
             while has_parentheses(dialect_entry) == True:
                 dialect_entry = re.sub(r'\([^()]*\)', '', dialect_entry)
+            
+            #Remove 'etc' annotation, which only appears in one entry of Egyptian Arabic
+            dialect_entry = re.sub(', etc.', '', dialect_entry)
             
             words = re.split('\s*[,;~/]\s*', dialect_entry) #split by ',', ';', or '~'
             for word in words:
@@ -318,6 +332,96 @@ def lookup_segment(segment, langs=None):
                 orth, tr, gloss = entry
                 print(f'<{orth}> /{tr}/ "{gloss}"')
             print('\n')
+
+#%%
+#Load annotated cognate codes
+gold_cognates = load_workbook('Source/arabic_gold_cognate_classes.xlsx')
+gold_cognates = gold_cognates['Sheet']
+gold_rows = list(range(1,len(list(gold_cognates.rows))+1))
+gold_columns = [i[0].column_letter for i in list(gold_cognates.columns)][:-3] #only include until column Q, which is the last row of data; beyond that is annotation key
+
+#Dictionary of language labels with the column they appear in
+language_columns = {gold_cognates[f'{c}1'].value:c for c in gold_columns[1:]}
+
+#Dictionary of concepts with rows of spreadsheet for each concept
+concept_rows = defaultdict(lambda:[])
+for r in gold_rows[1:]:
+    concept = gold_cognates[f'A{r}'].value.split('_')[0]
+    concept_rows[concept].append(r)
+
+
+#Skip singleton cognate classes for which the language has an entry in another non-singleton class
+singleton_rows = [r for r in gold_rows[1:]
+                  if len([gold_cognates[f'{c}{r}'].value for c in language_columns.values() 
+                          if gold_cognates[f'{c}{r}'].value != None]) == 1]
+singleton_skip = []
+for r in singleton_rows:
+    c = [c for c in language_columns.values() if gold_cognates[f'{c}{r}'].value != None][0]
+    concept = gold_cognates[f'A{r}'].value.split('_')[0]
+    concept_r = concept_rows[concept]
+    nonsingleton_concept_r = [row for row in concept_r if row not in singleton_rows]
+    for row in nonsingleton_concept_r:
+        if gold_cognates[f'{c}{row}'].value != None:
+            singleton_skip.append(r)
+            break
+singleton_skip = set(gold_cognates[f'A{r}'].value for r in singleton_skip)
+        
+
+#Create a dictionary with word forms and their cognate classes
+cognate_IDs = {}
+for language in language_columns:
+    c = language_columns[language]
+    
+    #Split by '~' for doublets, index by cognate ID
+    forms_ID = {gold_cognates[f'A{r}'].value:[form for form in gold_cognates[f'{c}{r}'].value.split('~')]
+        for r in gold_rows[1:]
+        if gold_cognates[f'{c}{r}'].value != None}
+    
+    #Add to cognate_IDs dict such that keys are (language, IPA, concept) and the values are the cognate ID
+    for cognate_ID in forms_ID:
+        concept = '_'.join(cognate_ID.split('_')[:-1])
+        for form in forms_ID[cognate_ID]:
+            cognate_IDs[(language, form, concept)] = cognate_ID
+            
+            
+#Iterate dataset and relabel cognate IDs as those from gold annotations  
+issues = []
+to_skip = []
+for i in processed_arabic_data:
+    entry = processed_arabic_data[i]
+    language, tr, concept = entry['Language_ID'], strip_ch(entry['Form'], ' '), entry['Parameter_ID']
+    try:
+        cognate_ID = cognate_IDs[(language, tr, concept)]
+        entry['Cognate_ID'] = cognate_ID
+        if cognate_ID in singleton_skip:
+            to_skip.append(i)
+    except KeyError:
+        print(f"Couldn't find cognate class for {language} /{tr}/ '{concept}'!")
+        issues.append((language, tr, concept))
+
+
+#Skip identified singleton classes by removing them from the dictionary after
+for i in to_skip:
+    del processed_arabic_data[i]
+
+
+#Mark (clear) loanwords
+def get_color(sheet, cell):
+    """Returns the hexadecimal color value of a particular cell, e.g. cell 'K44'"""
+    return str(sheet[cell].fill.start_color.index) #hexadecimal
+
+loanword_color = get_color(gold_cognates, 'S6')
+loanwords = [(l, gold_cognates[f'A{r}'].value)
+    for l in language_columns 
+    for r in gold_rows[1:]
+    if get_color(gold_cognates, f'{language_columns[l]}{r}') == loanword_color
+    ]
+for i in processed_arabic_data:
+    entry = processed_arabic_data[i]
+    language, cognate_id = entry['Language_ID'], entry['Cognate_ID']
+    if (language, cognate_id) in loanwords:
+        entry['Loan'] = 'TRUE'
+
 
 #%%
 #CREATE PROCESSED LANGUAGE FAMILY DOCUMENT
