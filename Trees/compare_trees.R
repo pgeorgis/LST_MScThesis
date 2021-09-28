5ffe3b567e619ca582fabf6e1e6c614c19a8a4f7
pgeorgis <60008814+pgeorgis@users.noreply.github.com>
2021-09-28 18:51:57 +0200

    Updated tree comparison script

--- Trees/compare_trees.R
+++ Trees/compare_trees.R
@@ -1,18 +1,93 @@
+#Load packages for loading and evaluating phylogenetic trees
 library(TreeDist)
 library(Quartet)
 library(phytools)
 
-gold_uralic <- '(((Livonian:2,((Estonian:2,Votic:2)Central_Finnic:1,((Veps:3,Ingrian:2,Karelian:2)Ladogan:1,Finnish:3)North_Finnic:1)Neva:1)Coastal_Finnic:1,Võro:2)Finnic:1,Hungarian:2,Khanty:4,Mansi:4,Meadow Mari:3,Erzya:2,((Komi-Permyak:2,Komi-Zyrian:1)Komi:1,Udmurt:2)Permian:1,((Skolt Saami:3,Kildin Saami:2)Eastern_Saami:1,(North Saami:3,South Saami:2)Western_Saami:1)Saami:1,(Tundra Nenets:4,Selkup:3,Nganasan:2)Samoyedic:1)Uralic;
-'
-gold_uralic = ape::read.tree(text = gold_uralic)
+#Set working directory to location where this script is saved
+library("rstudioapi")
+setwd(dirname(getActiveDocumentContext()$path))
+local_dir <- getwd()
 
+#Function for loading a series of Newick tree files into a MultiPhylo object
+load_forest <- function(newick_files) {
+  forest <- c()
+  for (file in newick_files) {
+    forest <- append(forest, read.newick(file))
+  }
+  return(forest)
+}
 
-my_uralic = '(((((Meadow Mari:4.13,Erzya:4.13):0.19,(((Skolt Saami:2.78,Kildin Saami:2.78):0.34,(North Saami:2.56,South Saami:2.56):0.56):0.97,((((Võro:1.82,Estonian:1.82):0.26,Votic:2.08):0.32,(((Karelian:1.13,Ingrian:1.13):0.26,Finnish:1.39):0.57,Veps:1.97):0.43):0.43,Livonian:2.83):1.27):0.23):0.14,((Komi-Permyak:1.40,Komi-Zyrian:1.40):1.16,Udmurt:2.56):1.90):0.13,((Mansi:3.67,Khanty:3.67):0.78,Hungarian:4.45):0.14):0.07,((Tundra Nenets:3.86,Nganasan:3.86):0.32,Selkup:4.18):0.48);'
-my_uralic = ape::read.tree(text = my_uralic)
+#Function for automatically evaluating a series of trees against one gold tree
+#Return the tree which best matches the gold tree
+best_matching_tree <- function(gold_tree, test_forest, forest_names=NULL) {
+  
+  #Create lists for TreeDist and QuartetDivergence scores
+  TD_scores <- c()
+  QD_scores <- c()
 
-#Note: gold_uralic had underscores where my_uralic had spaces; labels have to be identical
-#so I just changed the underscores to spaces manually so that the tips match
+  #Iterate through test trees and measure TreeDist and QuartetDivergence
+  for (test_tree in test_forest) {
+    tree_dist <- TreeDistance(gold_tree, test_tree)
+    statuses <- QuartetStatus(gold_tree, test_tree)
+    QD <- QuartetDivergence(statuses, similarity = FALSE)
+    TD_scores <- append(TD_scores, tree_dist)
+    QD_scores <- append(QD_scores, QD)
+  }
+  
+  #Get overall scores by averaging TreeDist and QuartetDivergence
+  scores <- (TD_scores + QD_scores) / 2
+  
+  #Get index of the tree with the minimum distance from the gold tree
+  min_index <- which.min(scores)
+  
+  #Print the name of the best tree if the corresponding names were supplied
+  if(!is.null(forest_names)) {
+    print(paste('Best match:', forest_names[[min_index]]))
+  }
+  
+  #Print the best tree's scores
+  print(paste('TreeDist:', round(TD_scores[[min_index]],2)))
+  print(paste('QuartetDivergence:', round(QD_scores[[min_index]],2)))
+  
+  #Return the tree with the minimum distance score from the gold tree
+  return(test_forest[[min_index]])
+}
 
-VisualizeMatching(ClusteringInfoDistance, gold_uralic, my_uralic)
-TreeDistance(gold_uralic, my_uralic)
-VisualizeQuartets(gold_uralic, my_uralic)
+#Get list of families
+families <- c('Arabic', 
+              'Balto-Slavic', 
+              'Dravidian',
+              'Hokan',
+              'Italic', 
+              'Polynesian', 
+              'Sinitic',
+              'Turkic', 
+              'Uralic')
+
+#Iterate through families, identify the best tree
+for (family in families) {
+
+  #Load gold Glottolog tree
+  family_gold <- read.newick(paste('Gold/', family, '_pruned.tre', sep=''))
+  
+  #Load distance-based trees into a MultiPhylo forest
+  #Filter only trees not based on gold cognate sets
+  family_trees <- list.files(path=paste('Results/', family, sep=''))
+  family_trees <- family_trees[grep('.tre', family_trees)]
+  family_trees <- family_trees[-grep('gold', family_trees)]
+  family_trees <- paste(local_dir, '/Results/', family, '/', family_trees, sep='')
+  family_forest <- load_forest(family_trees)
+  
+  #Remove full filepath from tree names
+  family_trees <- strsplit(family_trees, '/')
+  family_trees <- sapply(family_trees, tail, 1)
+  
+  #Evaluate and determine the best tree
+  best_tree <- best_matching_tree(family_gold, family_forest, family_trees)
+  
+  #Plot and save the evaluation of the best tree using QuartetDivergence diagram
+  plot_path <- paste('Results/', family, '/', family, "_best.png", sep='')
+  png(filename=plot_path, width=960, height=960)
+  VisualizeQuartets(ladderize(family_gold), ladderize(best_tree))
+  dev.off()
+}
